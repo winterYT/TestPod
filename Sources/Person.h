4d30f416d95bc1fb6f53eacd640a718f8e723380
290_100008737535_winter <290_100008737535_winter@git.cloud.tencent.com>
2019-03-18 17:03:41 +0800

    initial.

---
+++ Sources/Person.h
@@ -0,0 +1,20 @@
+//
+//  Person.h
+//  GCDTest
+//
+//  Created by winterJiao on 2019/3/15.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+- (void)eat;
+- (void)run;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  GCDTest
+//
+//  Created by winterJiao on 2019/3/15.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)eat {
+    NSLog(@"吃饭。。。。");
+}
+
+@end
